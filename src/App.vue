98f6311764f456059bb49cc383cac9491b6bc5ec
SuzuSys <sigmatics@outlook.jp>
2026-07-26 23:22:41 +0900

    feat: add app bar with title and theme toggle button to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <v-app>
+    <v-app-bar density="compact">
+      <v-app-bar-title>P2P Games</v-app-bar-title>
+      <template v-slot:append>
+        <v-btn
+          class="m-2"
+          icon="mdi-theme-light-dark"
+          @click="$vuetify.theme.cycle()"
+        />
+      </template>
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
