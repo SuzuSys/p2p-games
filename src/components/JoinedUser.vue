b0e655e9f93ad290e7f93445eb3c7632b7f73d8d
SuzuSys <sigmatics@outlook.jp>
2026-07-29 06:24:19 +0900

    feat: add JoinedUser component to display list of joined users

---
+++ src/components/JoinedUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card class="mx-auto my-5" max-width="640" rounded="lg" variant="outlined">
+    <v-card-item>
+      <v-card-title>Joined Users</v-card-title>
+    </v-card-item>
+    <v-card-text>
+      <div class="flex space-x-2">
+        <v-chip 
+          v-for="[clientId, value] in store.users" 
+          :key="clientId" 
+          variant="outlined"
+        >
+          {{ value.user.username }}
+        </v-chip>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+  import { useAppStore } from '@/stores/app'
+
+  const store = useAppStore()
+
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <form-username />
   <form-room v-if="!!store.username" />
+  <joined-user v-if="!!store.room" />
   <v-snackbar-queue v-model="store.snackbars" closable :timeout="3500" />
 </template>
 
 <script lang="ts" setup>
   import FormRoom from '@/components/FormRoom.vue'
   import FormUsername from '@/components/FormUsername.vue'
+  import JoinedUser from '@/components/JoinedUser.vue'
   import { useAppStore } from '@/stores/app'
 
   const store = useAppStore()
